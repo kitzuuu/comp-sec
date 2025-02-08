d579675ba920a4869730f71a510aecd40b978c74
Toma Cristian Nitu <toma.nitu1234@gmail.com>
2025-02-08 22:40:28 +0100

    Redesign

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { useRouter } from "next/navigation";
-
-export default function Home() {
-  const router = useRouter();
-
-      router.push("/login"); // Redirect unauthenticated users to login
-    
-}
