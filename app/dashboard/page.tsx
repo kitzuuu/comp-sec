cac260e0ec08d41ba3e040229af2990759f4c912
Huci Rares <rareshuci2@yahoo.com>
2025-02-27 16:21:30 +0100

    change dashboard background. modified the login logic to encrypt numbers also.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ export default function DashboardPage() {
     return (
         <div className="relative flex justify-center items-center min-h-screen bg-gray-100">
             {/* Background Image */}
-            <div className="absolute inset-0 bg-cover bg-center opacity-30" style={{ backgroundImage: "url('/stock-market.svg')" }}></div>
+            <div className="absolute inset-0 bg-cover bg-center opacity-30" style={{ backgroundImage: "url('/cristi.svg')" }}></div>
 
             {/* Dashboard Content */}
             <div className="relative z-10">
